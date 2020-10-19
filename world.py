b652d7324a96ac8173b7af9d8a0037f1b0e8e49c
Filip Palmqvist <fille.palmqvist@icloud.com>
2020-10-19 13:54:04 +0200

    made scrap interactable

--- world.py
+++ world.py
@@ -34,7 +34,8 @@ room_0_0 = {
             (125, 550), (50, 475), (50, 400), (0, 400), (0, 600), (800, 600), (800, 0)
         ]
     ],
-    "scrap_piles": spawn_random_scrap(amount=20, bounds=pygame.Rect((75, 75), (650, 450)))
+    "scrap_pile_coords": spawn_random_scrap(amount=20, bounds=pygame.Rect((75, 75), (650, 450))),
+    "scrap": []
 }
 
 room_0_1 = {
@@ -42,7 +43,8 @@ room_0_1 = {
         [(0, 0), (800, 0), (800, 200), (400, 200), (400, 400), (800, 400), (800, 600), (0, 600)]
     ],
     "ellipses": [(25, 25, 750, 550)],
-    "scrap_piles": [(350, 250), (450, 250), (350, 350), (450, 350)]
+    "scrap_pile_coords": [(350, 250), (450, 250), (350, 350), (450, 350)],
+    "scrap": []
 }
 
 room_0_2 = {
@@ -56,7 +58,8 @@ room_0_2 = {
             (0, 600)
         ]
     ],
-    "scrap_piles": [(150, 475), (300, 475)]
+    "scrap_pile_coords": [(150, 475), (300, 475)],
+    "scrap": []
 }
 
 room_1_0 = {
@@ -68,7 +71,8 @@ room_1_0 = {
             (800, 400), (800, 600), (500, 600), (500, 550), (550, 550)
         ]
     ],
-    "scrap_piles": [(725, 525), (225, 300)]
+    "scrap_pile_coords": [(725, 525), (225, 300)],
+    "scrap": []
 }
 
 room_1_1 = {
@@ -76,7 +80,8 @@ room_1_1 = {
         [(0, 0), (0, 200), (50, 200), (50, 50), (750, 50), (750, 550), (500, 550), (500, 600), (800, 600), (800, 0)],
         [(0, 400), (50, 400), (50, 550), (250, 550), (250, 200), (300, 200), (300, 600), (0, 600)]
     ],
-    "scrap_piles": [(150, 475), (625, 450)]
+    "scrap_pile_coords": [(150, 475), (625, 450)],
+    "scrap": []
 }
 
 room_1_2 = {
@@ -87,7 +92,8 @@ room_1_2 = {
             (750, 200), (500, 200), (500, 250), (250, 250), (250, 150), (750, 150), (750, 0)
         ]
     ],
-    "scrap_piles": [(625, 300), (375, 500)]
+    "scrap_pile_coords": [(625, 300), (375, 500)],
+    "scrap": []
 }
 
 room_2_0 = {
@@ -101,7 +107,8 @@ room_2_0 = {
             (750, 50), (550, 50), (550, 200), (500, 200), (500, 0)
         ]
     ],
-    "scrap_piles": [(150, 125), (150, 475), (650, 475), (650, 125)]
+    "scrap_pile_coords": [(150, 125), (150, 475), (650, 475), (650, 125)],
+    "scrap": []
 }
 
 room_2_1 = {
@@ -115,7 +122,8 @@ room_2_1 = {
             (150, 300), (150, 150), (500, 150)
         ]
     ],
-    "scrap_piles": [(700, 100), (700, 500), (250, 250)]
+    "scrap_pile_coords": [(700, 100), (700, 500), (250, 250)],
+    "scrap": []
 }
 
 room_2_2 = {
@@ -129,8 +137,15 @@ room_2_2 = {
             (450, 150), (500, 150), (500, 550), (750, 550), (750, 0)
         ]
     ],
-    "scrap_piles": [(325, 500), (475, 100)]
+    "scrap_pile_coords": [(325, 500), (475, 100)],
+    "scrap": []
 }
+
+rooms = [
+    [room_0_0, room_0_1, room_0_2],
+    [room_1_0, room_1_1, room_1_2],
+    [room_2_0, room_2_1, room_2_2]
+]
 
 
 def update_world():
@@ -152,8 +167,25 @@ def collide(obj1, obj2):
     return mask1.overlap_mask(mask2, (0, 0)).centroid() != (0, 0)
 
 
+def should_score(world_nr, player_rect):
+    room = rooms[world_nr[0]][world_nr[1]]
+    new_scrap_coords = []
+    score = 0
+    for scrap in room["scrap"]:
+        if not player_rect.colliderect(scrap):
+            new_scrap_coords.append(scrap.topleft)
+
+        else:
+            score = score + 1
+
+    room["scrap_pile_coords"] = new_scrap_coords
+    print(len(room["scrap_pile_coords"]))
+    return score
+
+
 def get_surface(room):
     surface = Surface((800, 600))
+    room["scrap"] = []
 
     for wall in room["walls"]:
         pygame.draw.polygon(surface, dark_green, wall)
@@ -162,13 +194,14 @@ def get_surface(room):
         for ellipse in room["ellipses"]:
             pygame.draw.ellipse(surface, beige, ellipse)
 
-    for scrap_center in room["scrap_piles"]:
+    for scrap_center in room["scrap_pile_coords"]:
         scrap = pygame.image.load(f"resources/mutter/mutter_{mutter_i}.png")
 
         scrap.set_colorkey(beige)
         scrap_rect = scrap.get_rect()
         scrap_rect.x, scrap_rect.y = scrap_center
 
+        room["scrap"].append(scrap_rect)
         surface.blit(scrap, scrap_rect)
 
     return surface
